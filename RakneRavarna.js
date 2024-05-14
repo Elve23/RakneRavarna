402bf64572cee8c2d84f2d073bdbf941fbb1f337
Degonatta <153090059+Degonatta@users.noreply.github.com>
2024-05-14 17:28:14 +0200

    kod js natalie test memoryfunktion

--- RakneRavarna.js
+++ RakneRavarna.js
@@ -7,3 +7,62 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidepanel").style.width = "0";
   }
+
+  // kod natalie spel :
+
+  const cards = document.querySelectorAll('.item');
+let firstCard = null;
+
+function flipCard(event) {
+    const clickedCard = event.currentTarget;
+
+    if(isCardLocked(clickedCard)){
+        return
+    }
+
+    clickedCard.classList.add('flip');
+
+    if (firstCard) {
+        // Om det finns ett första kort valt, jämför med det klickade kortet
+        if (firstCard.dataset.id !== clickedCard.dataset.id) {
+            // Om korten har olika id, vänd tillbaka dem efter en fördröjning
+            console.log("Korten har olika id, vänd tillbaka dem.");
+            setTimeout(() => {
+                firstCard.classList.remove('flip');
+                clickedCard.classList.remove('flip');
+                firstCard = null;
+            }, 1000);
+        } else {
+            lockCard(firstCard)
+            lockCard(clickedCard)
+            firstCard = null
+        }
+    
+    } else {
+        // Om det inte finns något första kort valt än, spara det klickade kortet som första kort
+        firstCard = clickedCard;
+    }
+}
+
+function lockCard(card){
+    card.classList.add('matched')
+}
+
+function isCardLocked(card){
+
+    let isCardLocked = card.classList.contains('matched')
+
+    return isCardLocked
+
+}
+
+
+
+function refreshPage() {
+    location.reload(); // Ladda om sidan
+}
+
+
+// Lägg till händelselyssnare för klick på varje kort
+cards.forEach(card => card.addEventListener('click', flipCard));
+document.getElementById("reloadButton").addEventListener("click", refreshPage);
